211052b03a3915fd84bd930320b4fde80aa6b7d3
Milad Remote <mniroee@gmail.com>
2023-11-26 10:14:28 +0330

    remove unused view

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/HomeView.vue'
-import About from '@/views/AboutView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', name: 'home', component: Home },
-    { path: '/about', name: 'about', component: About }
+    { path: '/', name: 'home', component: Home }
   ]
 })
 
