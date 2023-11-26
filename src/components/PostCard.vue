512c9246f01b9e4f9a20e5179c4a06859535df40
Milad Remote <mniroee@gmail.com>
2023-11-26 10:14:42 +0330

    add Post loading animation

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 import { reactive } from 'vue'
 
-defineProps(['user','users'])
+defineProps(['user', 'users'])
 
 const posts = reactive<{ id: number; userId: number; title: string; body: string }[]>([])
 
@@ -16,8 +16,6 @@ const getPosts = async (): Promise<void> => {
 
   posts.push(...userPosts)
   console.log(posts)
-
-  
 }
 
 getPosts()
@@ -29,6 +27,40 @@ const copy = () => {
 </script>
 
 <template>
+  <div class="animate-pulse" v-if="posts.length === 0">
+    <div class="bg-white w-100 mt-4 rounded-lg p-5 shadow-lg" v-for="i in 3" :key="i">
+      <div class="flex gap-5 mb-5">
+        <div class="rounded-lg w-[60px] h-[60px] bg-slate-200"></div>
+        <div>
+          <div class="h-4 w-20 bg-slate-200 rounded"></div>
+          <div class="h-3 w-[200px] bg-slate-200 rounded mt-3"></div>
+        </div>
+      </div>
+      <div class="mb-4">
+        <div class="h-4 max-w-[200px] bg-slate-200 rounded"></div>
+        <div class="h-3 w-full bg-slate-200 rounded mt-3"></div>
+        <div class="h-3 w-5/6 bg-slate-200 rounded mt-3"></div>
+        <div class="h-3 w-full bg-slate-200 rounded mt-3"></div>
+        <div class="h-3 w-1/2 bg-slate-200 rounded mt-3"></div>
+      </div>
+
+      <div class="flex gap-3">
+        <div class="h-4 w-4 bg-slate-200 rounded mt-3"></div>
+        <div class="h-4 w-1/2 bg-slate-200 rounded mt-3"></div>
+      </div>
+
+      <hr class="my-5" />
+
+      <div class="flex gap-3">
+        <div
+          v-for="j in 3"
+          :key="i + '-' + j"
+          class="rounded-lg w-[40px] h-[40px] bg-slate-200"
+        ></div>
+      </div>
+    </div>
+  </div>
+
   <article
     class="bg-white w-100 mt-4 rounded-lg p-5 shadow-lg"
     v-for="post in posts"
@@ -58,10 +90,19 @@ const copy = () => {
     <hr class="my-5" />
 
     <div class="flex gap-3">
-        <img v-for="user in ([...users].sort(() => 0.5 - Math.random())).slice(0,3)" :key="user.id" :src="user.picture.large" :alt="user.name.first" class="rounded-lg w-[40px] h-[40px]" />
-        <div @click="copy" class="text-center p-2 rounded-lg text-slate-700 bg-gray-200 w-[40px] h-[40px]">+{{ Math.floor(Math.random() * 10) }}</div>
-        
+      <img
+        v-for="user in [...users].sort(() => 0.5 - Math.random()).slice(0, 3)"
+        :key="user.id"
+        :src="user.picture.large"
+        :alt="user.name.first"
+        class="rounded-lg w-[40px] h-[40px]"
+      />
+      <div
+        @click="copy"
+        class="text-center p-2 rounded-lg text-slate-700 bg-gray-200 w-[40px] h-[40px]"
+      >
+        +{{ Math.floor(Math.random() * 10) }}
+      </div>
     </div>
-
   </article>
 </template>
